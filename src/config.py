96a078d12afed8de4fdcb60ab5817b67e5062606
Temporal Data <63362559+TemporalData@users.noreply.github.com>
2021-06-15 14:01:42 +0200

    Update
    Added way to generate images from the network
    
    Config which is training at the moment is in config and notes.txt

--- src/config.py
+++ src/config.py
@@ -2,21 +2,20 @@ import cv2
 import torch
 from math import log2
 
-START_TRAIN_AT_IMG_SIZE = 32
+START_TRAIN_AT_IMG_SIZE = 4
 DATASET = 'flickerHQ'
 CHECKPOINT_GEN = "generator.pth"
 CHECKPOINT_CRITIC = "critic.pth"
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 SAVE_MODEL = True
-LOAD_MODEL = True
+LOAD_MODEL = False
 LEARNING_RATE = 1e-3
-# BATCH_SIZES = [32, 32, 32, 16, 16, 16, 16, 8, 4]
-BATCH_SIZES = [32, 32, 32, 16, 16, 16] # Reduce for output 128
+BATCH_SIZES = [16, 16, 16, 16, 16, 16] # As paper
 CHANNELS_IMG = 3
-Z_DIM = 64  # should be 512 in original paper
-IN_CHANNELS = 64  # should be 512 in original paper
+Z_DIM = 128  # should be 512 in original paper
+IN_CHANNELS = 128  # should be 512 in original paper
 CRITIC_ITERATIONS = 1
 LAMBDA_GP = 10
-PROGRESSIVE_EPOCHS = [30] * len(BATCH_SIZES)
+PROGRESSIVE_EPOCHS = [25] * len(BATCH_SIZES)
 FIXED_NOISE = torch.randn(8, Z_DIM, 1, 1).to(DEVICE)
-NUM_WORKERS = 5
+NUM_WORKERS = 4
--- src/test.py
+++ src/test.py
@@ -1,3 +1,19 @@
 import torch
+import torch.optim as optim
+import config
+from model import Generator
+from utils import generate_examples, load_checkpoint
 
 print(torch.cuda.is_available())
+
+gen = Generator(
+        config.Z_DIM, config.IN_CHANNELS, img_channels=config.CHANNELS_IMG
+    ).to(config.DEVICE)
+
+opt_gen = optim.Adam(gen.parameters(), lr=config.LEARNING_RATE, betas=(0.0, 0.99))
+
+load_checkpoint(
+            config.CHECKPOINT_GEN, gen, opt_gen, config.LEARNING_RATE,
+        )
+
+generate_examples(gen, 4)
