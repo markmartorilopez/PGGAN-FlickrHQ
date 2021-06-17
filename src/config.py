0f2fd72f84cb7a41cf782c000f03142fbaf75d4a
Temporal Data <63362559+TemporalData@users.noreply.github.com>
2021-06-17 14:03:38 +0200

    Model trained

--- src/config.py
+++ src/config.py
@@ -2,13 +2,13 @@ import cv2
 import torch
 from math import log2
 
-START_TRAIN_AT_IMG_SIZE = 4
+START_TRAIN_AT_IMG_SIZE = 128
 DATASET = 'flickerHQ'
 CHECKPOINT_GEN = "generator.pth"
 CHECKPOINT_CRITIC = "critic.pth"
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 SAVE_MODEL = True
-LOAD_MODEL = False
+LOAD_MODEL = True
 LEARNING_RATE = 1e-3
 BATCH_SIZES = [16, 16, 16, 16, 16, 16] # As paper
 CHANNELS_IMG = 3
@@ -16,6 +16,6 @@ Z_DIM = 512  # should be 512 in original paper
 IN_CHANNELS = 512  # should be 512 in original paper
 CRITIC_ITERATIONS = 1
 LAMBDA_GP = 10
-PROGRESSIVE_EPOCHS = [100] * len(BATCH_SIZES)
+PROGRESSIVE_EPOCHS = [34] * len(BATCH_SIZES)
 FIXED_NOISE = torch.randn(8, Z_DIM, 1, 1).to(DEVICE)
 NUM_WORKERS = 5
